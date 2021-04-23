97880307521be7079eec9cbb706a1c19b4323b91
SangYoonLee <prestro1231@hufs.ac.kr>
2021-04-23 18:12:35 +0900

    Add files via upload

--- Python_DataStructureAlgorithm/Algorithm/Math/notation_stoi.py
+++ Python_DataStructureAlgorithm/Algorithm/Math/notation_stoi.py
@@ -6,11 +6,15 @@ def stoi(s, n):
     l = len(s)
     for i in range(l): ret += int(s[i]) * n ** (l-1-i)
     return ret
+# 계속되는 거듭제곱으로 연상량이 많아짐
 
 # 방법 2 (추천)
 def stoi(s, n):
     ret = 0
     for i in s: ret = int(i) + ret * n
+# 123 = 1 -> 10 -> 12 -> 120 -> 123
+# 123 = (((0 X 10 + 1) X 10) + 2) X 10 + 3
+# 거듭제곱 연산의 반복 최소화
 
 s = input()
 n = int(input())
--- Python_DataStructureAlgorithm/Algorithm/Math/prime_number.py
+++ Python_DataStructureAlgorithm/Algorithm/Math/prime_number.py
@@ -27,4 +27,15 @@ print(answer)
 # 에라토스테네스의 체
 # 1 ~ N까지의 소수를 찾는 방법 -> (1, 2 제외) sqrt(N)까지 수들의 배수를 지워나가는 방식
 def era(N):
-    
+    ck, p =[False for _ in range(N+1)], []
+    for i in range(2, N+1):
+        if ck[i] == True : continue
+        p.append(i)
+        for j in range(i*i, N+1, i):
+            ck[j] = True
+    return ck, p
+
+num = int(input())
+ck, p = era(num)
+#print(ck)
+print(p)
