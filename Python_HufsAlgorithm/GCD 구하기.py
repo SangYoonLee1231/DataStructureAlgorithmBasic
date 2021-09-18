6a85e55976e6e5788dc3a6c3dd8cd7c2e8939e6d
SYL (SangYoonLee) <sylagape1231@gmail.com>
2021-09-18 21:54:51 +0900

    Update GCD 구하기.py

--- Python_HufsAlgorithm/GCD 구하기.py
+++ Python_HufsAlgorithm/GCD 구하기.py
@@ -19,6 +19,14 @@ def gcd_sub(a, b):
 			b = temp
 	return a
 
+# 뺄셈을 이용하여 구현2
+def gcd_sub2(a, b):
+	while (a != 0 and b != 0) :
+		if a > b: a = a - b
+		else: b = b - a
+	return a + b
+
 a, b = map(int, input().split())
 print(gcd_mod(a, b))
 print(gcd_sub(a, b))
+print(gcd_sub2(a, b))
